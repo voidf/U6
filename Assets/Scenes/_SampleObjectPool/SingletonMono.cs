8108cdaead926800c4e6e998548e8bbdc8dd7ca5
voidf <510030805@qq.com>
2024-07-18 21:05:55 +0800

    upload bak

---
+++ Assets/Scenes/_SampleObjectPool/SingletonMono.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMono<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get {
+            return instance;
+        }
+    }
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scenes/_SampleObjectPool/ThePool.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ThePool : SingletonMono<ThePool>
+{
+    // 子弹预制体
+    public BulletOrSomethingSpawnFrequently bulletPrefab;
+
+    // 对象池
+    private ObjectPool<BulletOrSomethingSpawnFrequently> bulletPool;
+
+    private new void Awake()
+    {
+        base.Awake();
+        bulletPool = new ObjectPool<BulletOrSomethingSpawnFrequently>(CreateBullet, OnTakeFromPool, OnReturnedToPool, OnDestroyBullet, maxSize: 200);
+    }
+
+    private BulletOrSomethingSpawnFrequently CreateBullet()
+    {
+        return Instantiate(bulletPrefab);
+    }
+
+    private void OnTakeFromPool(BulletOrSomethingSpawnFrequently BulletOrSomethingSpawnFrequently)
+    {
+        BulletOrSomethingSpawnFrequently.gameObject.SetActive(true);
+    }
+
+    private void OnReturnedToPool(BulletOrSomethingSpawnFrequently BulletOrSomethingSpawnFrequently)
+    {
+        BulletOrSomethingSpawnFrequently.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyBullet(BulletOrSomethingSpawnFrequently BulletOrSomethingSpawnFrequently)
+    {
+        Destroy(BulletOrSomethingSpawnFrequently.gameObject);
+    }
+    public void ReturnBullet(BulletOrSomethingSpawnFrequently BulletOrSomethingSpawnFrequently)
+    {
+        bulletPool.Release(BulletOrSomethingSpawnFrequently);
+    }
+    public void ShootBullet(Vector3 position, Vector3 direction)
+    {
+        BulletOrSomethingSpawnFrequently BulletOrSomethingSpawnFrequently = bulletPool.Get();
+        BulletOrSomethingSpawnFrequently.transform.position = position;
+        BulletOrSomethingSpawnFrequently.Shoot(direction);
+    }
+}
